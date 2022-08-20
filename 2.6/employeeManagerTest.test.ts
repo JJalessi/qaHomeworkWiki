ae8035910ad30174a230273cd4ebc258819195ab
JJalessi <jjalessi.official@gmail.com>
2022-08-19 18:03:00 -0700

    2.6 Homework

--- 2.6/employeeManagerTest.test.ts
+++ 2.6/employeeManagerTest.test.ts
@@ -8,12 +8,25 @@ import {Builder,By, Capabilities, until, WebDriver, } from "selenium-webdriver";
   class employeePage {
       driver: WebDriver;
       url: string = "https://devmountain-qa.github.io/employee-manager/1.2_Version/index.html";
-        //FILL OUT LOCATORS CONSTRUCTOR AND METHODS IN ORDER TO PASS THE TEST
+      header: By = By.css(".titleText") 
+      addEmployee: By = By.name("addEmployee")
+      newEmployee: By = By.name("employee11")
+      nameInput: By = By.name("nameEntry")
+      phoneInput: By = By.name("phoneEntry")
+      titleInput: By = By.name("titleEntry")
+      constructor(driver: WebDriver) {
+        this.driver = driver
+      }
+     async navigate () {
+        await this.driver.get(this.url)
+        await this.driver.wait(until.elementLocated(this.header)) 
+     }
   }
+  const emPage = new employeePage(driver)
 
   describe("Employee Manger Test", () => {
       beforeEach(async () => {
-          await employeePage.navigate();
+          await emPage.navigate();
       })
       afterAll(async () => {
           await driver.quit()
@@ -33,4 +46,4 @@ import {Builder,By, Capabilities, until, WebDriver, } from "selenium-webdriver";
           await driver.findElement(emPage.titleInput).sendKeys("Change this")
   })
 
-  /* this is a commment */
+}) 
